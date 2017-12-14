eae242c30007c6be20f54c190501ab6e4747cf0e
Tristen Allen <spyboticsguy@gmail.com>
2017-12-14 15:14:22 -0500

    Add barebones loading info to main plugin.

--- src/main/java/com/tristenallen/actiondialogue/ActionDialogue.java
+++ src/main/java/com/tristenallen/actiondialogue/ActionDialogue.java
@@ -1,6 +1,18 @@
 package com.tristenallen.actiondialogue;
 
+import com.tristenallen.actiondialogue.dialogue.DialogueManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ActionDialogue extends JavaPlugin {
+    private DialogueManager dialogueManager;
+
+    @Override
+    public void onEnable() {
+        dialogueManager = new DialogueManager(this);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 }
